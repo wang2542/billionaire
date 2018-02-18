21a6d78aac31c5cf91622a52fa14fd2ce309e8b9
Shirley Fu <fu157@purdue.edu>
2018-02-18 16:22:10 -0500

    adding the news model

--- file/myapp/app.js
+++ file/myapp/app.js
@@ -8,23 +8,7 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-var news
 
-
-//get news api
-const webhoseio = require('webhoseio');
-const client = webhoseio.config({token: 'cf0134db-bd58-480c-b9ab-2aa7e552dab5'});
-const query_params = {
-  "q": "“stocks” language:english rating:>4",
-  "sort": "crawled"
-}
-client.query('filterWebContent', query_params)
-  .then(output => {
-    news = output;
-    console.log(news);
-    //console.log(output['posts'][0]['title']); // Print the text of the first post
-    console.log(output['posts'][0]['published']); // Print the text of the first post publication date
-});
 
 
 // view engine setup
@@ -43,28 +27,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Store all HTML files in view folder.
 app.use(express.static(path.join(__dirname, 'views')));
 
-//app.use('/', index);
+app.use('/', index);
 //app.use('/users', users);
-app.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname + '/views/index-6.html'));
-
-});
 
 
-app.get('/News', function(req, res) {
-    console.log("sss");
-    if(req.url!=="/favicon.ico"){
-        res.writeHead(200,{"Content-Type":"text/plain","Access-Control-Allow-Origin":"http://localhost"});
-        var json = JSON.stringify({ 
-           title:news['posts'][0]['title'],
-           image:news['posts'][0]['main_image'],
-          //anArray: ["item1", "item2"], 
-          //another: "item"
-        });
-        console.log(json.image);
-    }
-    res.end(json);
-});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ file/myapp/model/news.js
@@ -0,0 +1,22 @@
+
+var express = require('express');
+
+const webhoseio = require('webhoseio');
+const client = webhoseio.config({token: 'cf0134db-bd58-480c-b9ab-2aa7e552dab5'});
+var bodyParser = require('body-parser');
+
+var query_params = {
+  "q": "“stocks” language:english rating:>4",
+  "sort": "crawled"
+}
+
+module.exports.getNews = function(callback) {
+ client.query('filterWebContent', query_params)
+  .then((output) => {
+    news = JSON.parse(output);
+    console.log(JSON.stringify(news));
+    //console.log(output['posts'][0]['title']); // Print the text of the first post
+    console.log(output['posts'][0]['published']); // Print the text of the first post publication date
+    callback(err,news);
+});
+}
