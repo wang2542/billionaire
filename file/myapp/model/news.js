ca4dcc4fc41fde802007d274c640163580f62f07
rui pang <ruipang1122@gmail.com>
2018-02-19 21:03:21 -0500

    news

--- file/myapp/model/news.js
+++ file/myapp/model/news.js
@@ -1,5 +1,5 @@
-
 var express = require('express');
+var out;
 
 const webhoseio = require('webhoseio');
 const client = webhoseio.config({token: 'cf0134db-bd58-480c-b9ab-2aa7e552dab5'});
@@ -9,13 +9,14 @@ var query_params = {
   "q": "“stocks” language:english rating:>4",
   "sort": "crawled"
 }
-
 module.exports.getNews = function(callback) {
  client.query('filterWebContent', query_params)
   .then((output) => {
-    console.log(output);
+   // console.log(output);
+    console.log("go api");
     //console.log(output['posts'][0]['title']); // Print the text of the first post
-    console.log(output['posts'][0]['published']); // Print the text of the first post publication date
+    //console.log(output['posts'][0]['published']); // Print the text of the first post publication date
     callback(output);
 });
 }
+
--- file/myapp/routes/index.js
+++ file/myapp/routes/index.js
@@ -1,35 +1,20 @@
 var express = require('express');
 var router = express.Router();
-var news = require('../model/news');
+var news = require('../model/news.js');
 var bodyParser = require('body-parser');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
- 
-  
-  res.render('index-6', { title: 'Express' });
-  
-});
-
-router.get('/News', function(req, res) {
   console.log("sss");
-  var json;
-  news.getNews(function(news){
-    console.log(news);
+  //console.log(news.value);
+  
+  
+});
 
-   if(req.url!=="/favicon.ico"){
-    res.writeHead(200,{"Content-Type":"text/plain","Access-Control-Allow-Origin":"http://localhost"});
-      json = JSON.stringify({ 
-       title:news['posts'][0]['title'],
-       image:news['posts'][0]['main_image'],
-      //anArray: ["item1", "item2"], 
-      //another: "item"
-    });
-    console.log(json.image);
-   }
-   res.end(json.title);
-  });
- 
+router.get('/news', function(req, res) {
+
  
 });
+ 
+ 
 module.exports = router;
