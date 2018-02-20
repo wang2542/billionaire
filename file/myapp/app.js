369baa6a5d03b28ca2213c07a83a866a4d5b6564
Shirley (Xiaowei) Fu <sfu@Shirleys-iMac.local>
2018-02-19 21:57:16 -0500

    connect db with app

--- file/myapp/app.js
+++ file/myapp/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var mongoose = require('mongoose');
+var stock = require('./model/stock');  
 mongoose.connect('mongodb://localhost/cs407');
 
 
@@ -38,6 +39,9 @@ app.use(express.static(path.join(__dirname, 'views')));
 
 app.use('/', index);
 
+stock.init( function() {
+   console.log('stock initializing');
+});
 
 /*
 // catch 404 and forward to error handler
